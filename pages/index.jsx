b27e10f69be95bc9508d009442dbbeec163a9f85
Slip <72151847+slip1244@users.noreply.github.com>
2021-12-31 22:42:11 -0600

    min-height hero section

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ const Landing = () => (
         </div>
         <style jsx>{`
             .landing {
-                height: calc(100vh - 80px);
+                min-height: calc(100vh - 80px);
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
