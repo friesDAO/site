aee2dce51d30f3c7558f1b3d1769bb4f60e43438
Slip <72151847+slip1244@users.noreply.github.com>
2022-02-04 01:42:03 -0600

    fixes + next wl batch + nft pending stats

--- state/useRaise.js
+++ state/useRaise.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import constants from "../data/constants.json"
 import { makeLeaf, makeTree } from "../util/merkle.js"
+const fetch = require("node-fetch")
 const whitelist = require(`../data/${constants.whitelist}.json`)
 
 const tree = makeTree(whitelist)
@@ -16,12 +17,19 @@ function useRaise(account, Sale, BN, toWei, fromWei) {
 
     const [ amountPurchased, setAmountPurchased ] = useState(0)
     const [ whitelistMax, setWhitelistMax ] = useState(0)
+    const [ nftsRemaining, setNftsRemaining] = useState(0)
 
 
     useEffect(() => {
         updateData()
         const interval = setInterval(updateData, 5000)
-        return () => clearInterval(interval)
+
+        updateNFTsRemaining()
+        const interval2 = setInterval(updateData, 20000)
+        return () => {
+            clearInterval(interval)
+            clearInterval(interval2)
+        }
     }, [account])
 
     async function updateData() {
@@ -57,6 +65,14 @@ function useRaise(account, Sale, BN, toWei, fromWei) {
         }
     }
 
+    async function updateNFTsRemaining() {
+        const contributions = await fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${constants.startBlock}&toBlock=latest&address=${constants.sale}&topic0=${constants.purchaseTopic}&apikey=${constants.etherscanAPI}`).then(res => res.json())
+        if (contributions.status == 1) {
+            console.log(contributions.result.length)
+            setNftsRemaining(250 - contributions.result.length)
+        }
+    }
+
     return {
         whitelistSaleActive,
         publicSaleActive,
@@ -67,7 +83,8 @@ function useRaise(account, Sale, BN, toWei, fromWei) {
         amountPurchased,
         setAmountPurchased,
         whitelistMax,
-        tree
+        tree,
+        nftsRemaining
     }
 }
 
