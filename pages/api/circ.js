ee4bad8d5dc7ecdc1d52c0a3fbe9a8848f5eb1ba
Slip <72151847+slip1244@users.noreply.github.com>
2022-03-01 18:08:58 -0600

    circulating supply endpoint

---
+++ pages/api/circ.js
@@ -0,0 +1,11 @@
+export default async (req, res) => {
+    if (req.method == 'GET') {
+        const totalSupply = 840000000
+
+        const circAllocations = {
+            raise: 233961218.37375628719,
+        }
+
+        res.status(200).send(Object.values(circAllocations).reduce((c, i) => c + i));
+    }
+};
